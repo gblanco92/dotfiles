5544f655503cf115f672c0acf50c800084ff8a00
Guillem Blanco <gblanco92@gmail.com>
2017-03-27 15:34:00 +0200

    start using ftplugin

--- install.sh
+++ install.sh
@@ -10,3 +10,13 @@ rm -f ~/.vimrc && ln -s $DIR/init.vim ~/.vimrc
 rm -f ~/.tmux.conf && ln -s $DIR/tmux.conf ~/.tmux.conf
 rm -f ~/.zshrc && ln -s $DIR/zshrc ~/.zshrc
 rm -f ~/.gitconfig && ln -s $DIR/gitconfig ~/.gitconfig
+
+mkdir -p ~/.config/nvim/ftplugin
+mkdir -p ~/.config/nvim/ftplugin/tex
+mkdir -p ~/.vim/ftplugin
+mkdir -p ~/.vim/ftplugin/tex
+
+rm -f ~/.config/nvim/ftplugin/cpp.vim && ln -s $DIR/ftplugin/cpp.vim ~/.config/nvim/ftplugin
+rm -f ~/.config/nvim/ftplugin/tex/* && ln -s $DIR/ftplugin/tex/* ~/.config/nvim/ftplugin/tex
+rm -f ~/.vim/ftplugin/cpp.vim && ln -s $DIR/ftplugin/cpp.vim ~/.vim/ftplugin
+rm -f ~/.vim/ftplugin/tex/* && ln -s $DIR/ftplugin/tex/* ~/.vim/ftplugin/tex
